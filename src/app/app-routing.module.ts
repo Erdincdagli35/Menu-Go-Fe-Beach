0f781aec39417b4aa55fde08e438859b6c27049d
erdincdagli35 <3rd1fbslash@gmail.com>
2026-04-14 00:42:15 +0300

    getAll api link has been updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { AdminProductControlComponent } from './admin-product-control/admin-product-control.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'menu/plaj', pathMatch: 'full' },
-  { path: 'menu/plaj', component: BeachMenuComponent },
+  { path: '', redirectTo: 'menu//beach', pathMatch: 'full' },
+  { path: 'menu//beach', component: BeachMenuComponent },
   { path: 'admin/login', component: LoginPageComponent },
   { path: 'admin/product-control', component: AdminProductControlComponent },
-  { path: '**', redirectTo: 'menu/plaj' }
+  { path: '**', redirectTo: 'menu//beach' }
 ];
 
 @NgModule({
