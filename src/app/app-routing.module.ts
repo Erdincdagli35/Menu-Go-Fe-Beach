9b60223a1afe3240b9800c7964cacee65c7e0844
erdincdagli35 <3rd1fbslash@gmail.com>
2026-04-14 20:37:58 +0300

    create api has been integrated v.2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { BeachMenuComponent } from './beach-menu/beach-menu.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { AdminProductControlComponent } from './admin-product-control/admin-product-control.component';
+import { MenuCreateComponent } from './menu-create/menu-create.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'menu/beach', pathMatch: 'full' },
   { path: 'menu/beach', component: BeachMenuComponent },
   { path: 'admin/login', component: LoginPageComponent },
   { path: 'admin/product-control', component: AdminProductControlComponent },
+  { path: 'menu/create', component: MenuCreateComponent },
   { path: '**', redirectTo: 'menu/beach' }
 ];
 
